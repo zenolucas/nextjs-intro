851b5e336ab487f4bf1432ccb13f8576c06ea411
Zen Lucas <2200501@slu.edu.ph>
2025-04-28 21:51:03 +0800

    setup DaisyUI and initial login page

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,7 +1,17 @@
 export default function Login() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <div className="flex flex-col items-center justify-center min-h-screen p-4">
+      <h1 className="text-3xl font-bold mb-6">
         This is the login page
-    </h1>
-  )
+      </h1>
+
+      <fieldset className="fieldset w-full max-w-sm">
+        <legend className="fieldset-legend text-lg font-medium mb-2">
+          What is your name?
+        </legend>
+        <input type="text" className="input input-bordered w-full mb-2" placeholder="Type here" />
+        <p className="label text-sm text-gray-500">Optional</p>
+      </fieldset>
+    </div>
+  );
 }
